19ad7ada6e51754aca683d91e52fbdc1870fb630
michalkurka <michalk@h2o.ai>
2018-02-09 15:45:25 -0800

    Refactor NativeLibLoader to allow 3rd party loaders provided by SPI

--- jvm-packages/xgboost4j/src/main/java/ml/dmlc/xgboost4j/java/NativeLibraryLoaderChain.java
+++ jvm-packages/xgboost4j/src/main/java/ml/dmlc/xgboost4j/java/NativeLibraryLoaderChain.java
@@ -16,69 +16,118 @@
 package ml.dmlc.xgboost4j.java;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import static ml.dmlc.xgboost4j.java.NativeLibrary.CompilationFlags.WITH_GPU;
+import static ml.dmlc.xgboost4j.java.NativeLibrary.CompilationFlags.WITH_OMP;
+import static ml.dmlc.xgboost4j.java.NativeLibrary.EMPTY_COMPILATION_FLAGS;
+import static ml.dmlc.xgboost4j.java.NativeLibrary.nativeLibrary;
+
 /**
  * A simple loader which tries to load all
- * specified libraries in given order.
+ * specified libraries in a given order.
  */
-public class NativeLibraryLoaderChain implements Loadable {
+public class NativeLibraryLoaderChain implements INativeLibLoader {
 
   private static final Log logger = LogFactory.getLog(NativeLibraryLoaderChain.class);
 
   private final Loadable[] nativeLibs;
 
-  private Loadable succesfullyLoaded = null;
+  private Loadable successfullyLoaded = null;
 
-  private boolean loaded = false;
+  public NativeLibraryLoaderChain() {
+    this(
+      // GPU support enabled
+      nativeLibrary("xgboost4j_gpu", new NativeLibrary.CompilationFlags[] {WITH_GPU, WITH_OMP}),
+      // OMP support enabled
+      nativeLibrary("xgboost4j_omp", new NativeLibrary.CompilationFlags[] {WITH_OMP}),
+      // Minimum version of library - no gpu, no omp
+      nativeLibrary("xgboost4j_minimal", EMPTY_COMPILATION_FLAGS)
+    );
+  }
 
-  private NativeLibraryLoaderChain(Loadable[] libs) {
+  private NativeLibraryLoaderChain(Loadable... libs) {
     assert libs != null : "Argument `libs` cannot be null.";
     nativeLibs = libs;
   }
 
   @Override
-  public synchronized boolean load() throws IOException {
-    if (!isLoaded()) {
-      ArrayList<IOException> exs = new ArrayList<>();
-      for (Loadable lib : nativeLibs) {
-        try {
-          // Try to load
-          if (loaded = lib.load()) {
-            // It was successful load, so remember it
-            succesfullyLoaded = lib;
-            break;
-          }
-        } catch (IOException e) {
-          logger.info("Cannot load library: " + lib.toString());
-          exs.add(e);
+  public void loadNativeLibs() throws IOException {
+    LinkedList<IOException> exs = new LinkedList<>();
+    for (Loadable lib : nativeLibs) {
+      try {
+        // Try to load
+        if (lib.load()) {
+          // It was successful load, so remember it
+          successfullyLoaded = lib;
+          break;
         }
-      }
-      if (!isLoaded() && !exs.isEmpty()) { // Try to load something but failed
-        throw new IOException(exs.get(exs.size()-1));
+      } catch (IOException e) {
+        logger.info("Cannot load library: " + lib.toString());
+        exs.add(e);
       }
     }
-    return isLoaded();
+    if ((successfullyLoaded == null) && (! exs.isEmpty())) {
+      throw new IOException(exs.getLast());
+    }
   }
 
   @Override
-  public boolean isLoaded() {
-    return loaded;
+  public String name() {
+    return "NativeLibraryLoaderChain";
   }
 
   @Override
-  public String getName() {
-    return "nativeLibrartLoaderChain";
+  public int priority() {
+    return 1;
   }
 
-  public static NativeLibraryLoaderChain loaderChain(Loadable ...libs) {
-    return new NativeLibraryLoaderChain(libs);
+  /**
+   * Full name of loaded library.
+   * @return  library name (e.g., 'xgboost4j_gpu`), never returns null.
+   * @throws IOException  if no library was found or library loading fails
+   */
+  public String getLoadedLibraryName() throws IOException {
+    if (successfullyLoaded != null) {
+      return successfullyLoaded.getName();
+    } else {
+      throw new IOException("No binary library found!");
+    }
   }
 
-  public Loadable getSuccesfullyLoaded() {
-    return succesfullyLoaded;
+  /**
+   * Returns suffix of library which marks binary version (gpu, omp)
+   *
+   * @return  library suffix
+   * @throws IOException  if no library was found, or library loading fails, or suffix cannot
+   * be extracted
+   */
+  @Deprecated
+  public String getLoadedLibrarySuffix() throws IOException {
+    String libName = getLoadedLibraryName();
+    int lastIndex;
+    if (libName != null && (lastIndex = libName.lastIndexOf('_')) >= 0) {
+      return libName.substring(lastIndex + 1);
+    } else {
+      throw new IOException("Inconsistency in library name detected! libName = " + libName);
+    }
   }
+
+  /** Return compilation flags for loaded library or throws exception if no library was loaded
+   *
+   * @return  compilation flags of loaded XGBoost library
+   * @throws IOException  if no XGBoost library was found
+   */
+  public NativeLibrary.CompilationFlags[] getLoadedLibraryCompilationFlags() throws IOException {
+    if (successfullyLoaded != null) {
+      return ((NativeLibrary) successfullyLoaded).getCompilationFlags();
+    } else {
+      throw new IOException("No binary library found!");
+    }
+  }
+
+
 }

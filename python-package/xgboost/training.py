172db33446d05da6ab3f617ab25ccefaf52e204a
pseudotensor <pseudotensor@gmail.com>
2018-04-21 16:16:27 -0700

    Directly use booster if not string for warm-start or continuation

--- python-package/xgboost/training.py
+++ python-package/xgboost/training.py
@@ -35,8 +35,10 @@ def _train_internal(params, dtrain,
 
     if xgb_model is not None:
         if not isinstance(xgb_model, STRING_TYPES):
+            bst = xgb_model # need to make copy in general?
+        else:
             xgb_model = xgb_model.save_raw()
-        bst = Booster(params, [dtrain] + [d[0] for d in evals], model_file=xgb_model)
+            bst = Booster(params, [dtrain] + [d[0] for d in evals], model_file=xgb_model)
         nboost = len(bst.get_dump())
 
     _params = dict(params) if isinstance(params, list) else params

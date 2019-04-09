40ab1fbc266644942db85c640ef733b29df0c9f2
Michal Raška <michal.raska@gmail.com>
2019-04-09 10:23:37 +0200

    Forgotten change.

--- ci/Jenkinsfile.groovy
+++ ci/Jenkinsfile.groovy
@@ -27,8 +27,8 @@ XGB_VERSION = "${XGB_MAJOR_VERSION}.${currentBuild.number}"
 
 def targetNexus = params.targetNexus ?: TARGET_NEXUS_NONE
 targetNexus = targetNexus.toLowerCase()
+env.SAFE_BRANCH_NAME = env.BRANCH_NAME.replaceAll('/|\\ ', '-').toLowerCase()
 if (env.BRANCH_NAME != PUBLISHABLE_BRANCH_NAME) {
-    env.SAFE_BRANCH_NAME = env.BRANCH_NAME.replaceAll('/|\\ ', '-').toLowerCase()
     XGB_VERSION = "0.8.${currentBuild.number}-${env.SAFE_BRANCH_NAME}-SNAPSHOT"
     if (targetNexus != TARGET_NEXUS_NONE) {
         targetNexus = TARGET_NEXUS_SNAPSHOT
@@ -225,7 +225,7 @@ private GString getStageDirFromConfiguration(final config) {
 private void s3Upload(final String folder, final String file, final String targetNexus) {
     def s3Root = "s3://test.0xdata.com/h2o-release/xgboost/${env.SAFE_BRANCH_NAME}/${XGB_VERSION}"
     if (targetNexus == TARGET_NEXUS_PUBLIC) {
-        s3Root = "s3://h2o-release/xgboost/${env.BRANCH_NAME}/${XGB_VERSION}"
+        s3Root = "s3://h2o-release/xgboost/${env.SAFE_BRANCH_NAME}/${XGB_VERSION}"
     }
     docker.withRegistry("https://docker.h2o.ai") {
         withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'AWS S3 Credentials', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {

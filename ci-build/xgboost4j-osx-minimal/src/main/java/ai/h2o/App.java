f26300caaca19945fcd1da7f40f97c1a9b43b084
Jan Sterba <info@jansterba.com>
2020-09-23 19:54:39 +0200

    docker image, cmake and devtoolset upgrade

--- ci-build/xgboost4j-osx-minimal/src/main/java/ai/h2o/App.java
+++ ci-build/xgboost4j-osx-minimal/src/main/java/ai/h2o/App.java
@@ -2,9 +2,9 @@ package ai.h2o;
 
 public class App {
 
-    public static final String VERSION = "!SUBST_VERSION";
-    public static final String BACKEND = "!SUBST_BACKEND";
-    public static final String OS = "!SUBST_OS";
+    public static final String VERSION = "1.2.0-SNAPSHOT";
+    public static final String BACKEND = "minimal";
+    public static final String OS = "osx";
 
     public static void main(String[] args) {
         System.out.println(String.format("XGBoost Library Details:\n\tVersion: %s\n\tBackend: %s\n\tOS: %s", VERSION, BACKEND, OS));
